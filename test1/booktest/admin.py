337418e26f1f03aa0efb5587aa62a580f517bb9a
xwp <1250823345@qq.com>
2018-03-20 20:46:12 +0800

    后台管理

--- test1/booktest/admin.py
+++ test1/booktest/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['pk','btitle','bpub_date']
+    list_filter = ['btitle']
+    search_fields = ['btitle']
+    list_per_page = 1
+    fieldsets = [
+        ('base',{'fields':['btitle']}),
+        ('super',{'fields':['bpub_date']})
+    ]
+
+
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo)
--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -6,7 +6,7 @@ class BookInfo(models.Model):
     btitle=models.CharField(max_length=20)
     bpub_date=models.DateField()
     def __str__(self):
-        return self.btitle
+        return self.btitle.encode('utf-8')
 
 
 class HeroInfo(models.Model):
